76318b4cf4b217133ca3066337eb21ce763f690b
Krstan Vjestica <krle@tilda.center>
2019-10-01 14:07:11 +0200

    install docker and setup url with port

--- jenkins.sh
+++ jenkins.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
+echo "Pre-Install common"
+sudo apt-get -y install apt-transport-https ca-certificates curl gnupg-agent software-properties-common > /dev/null 2>&1
+
 echo "Adding apt-keys"
 wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -
 echo deb http://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
 echo "Updating apt-get"
 sudo apt-get -qq update
@@ -17,12 +21,24 @@ sudo apt-get -y install git > /dev/null 2>&1
 echo "Installing git-ftp"
 sudo apt-get -y install git-ftp > /dev/null 2>&1
 
+echo "Installing docker"
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" > /dev/null 2>&1
+sudo apt-get update > /dev/null 2>&1
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io > /dev/null 2>&1
+
+echo "Enable and starting Docker"
+sudo service docker start
+
 echo "Installing jenkins"
 sudo apt-get -y install jenkins > /dev/null 2>&1
 sudo service jenkins start
 
 sleep 1m
 
-echo "Installing Jenkins Plugins"
+echo "Password is:"
 JENKINSPWD=$(sudo cat /var/lib/jenkins/secrets/initialAdminPassword)
 echo $JENKINSPWD
+
+echo "URL address"
+URL=$(sudo ip -4 addr show enp0s8 | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
+echo "http://"$URL":8080"
