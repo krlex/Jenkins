ba728539ad700412cfb8d1377f4618e12d2aacce
Krstan Vjestica <krle@tilda.center>
2019-10-03 15:00:00 +0200

    add in installation ansible, yarn and nodejs

--- jenkins.sh
+++ jenkins.sh
@@ -1,12 +1,19 @@
 #!/usr/bin/env bash
 
 echo "Pre-Install common"
-sudo apt-get -y install apt-transport-https ca-certificates curl gnupg-agent software-properties-common > /dev/null 2>&1
+sudo apt-get -y install apt-transport-https ca-certificates curl gnupg-agent software-properties-common gcc g++ make tmux python3-pip > /dev/null 2>&1
 
 echo "Adding apt-keys"
 wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -
 echo deb http://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash - /dev/null 2>&1
+curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+
+
+echo "Installation Ansible"
+pip3 install -y ansible > /dev/null 2>&1
 
 echo "Updating apt-get"
 sudo apt-get -qq update
@@ -14,6 +21,9 @@ sudo apt-get -qq update
 echo "Installing default-java"
 sudo apt-get -y install default-jre > /dev/null 2>&1
 sudo apt-get -y install default-jdk > /dev/null 2>&1
+
+echo "Install npm and yarn"
+sudo apt-get install yarn nodejs -y > /dev/null 2>&1
 
 echo "Installing git"
 sudo apt-get -y install git > /dev/null 2>&1
